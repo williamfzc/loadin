a2d02e5a2a482ce701f105b83850650aa0ee3d0c
williamfzc <178894043@qq.com>
2018-01-20 22:11:51 +0800

    update README

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='loadin',
-    version='0.0.1',
+    version='0.1',
     description='quite a easy way to apply animation to your function',
     author='Feng Zhangchi',
     author_email='fengzc@vip.qq.com',
     url='https://github.com/williamfzc/loadin',
-    py_modules=['loadin'],
 )
